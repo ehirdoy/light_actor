49dd57facf9d5eaab06a34fc2e1228f0e899ee2b
Hiroshi Doyu <hiroshi.doyu@ericsson.com>
2019-01-28 07:41:01 +0000

    HELPME: fixing unkown Actor_param_types
    $ make
    $ make install
    $ cd test/mirage
    $ bash buildit unix
    mirage configure -t unix
    make depend
    make[1]: Entering directory '/home/ehirdoy/src/light_actor/test/mirage'
    opam pin add -k path --no-action --yes mirage-unikernel-actor-unix . && opam depext --yes --update mirage-unikernel-actor-unix ; opam pin remove --no-action mirage-unikernel-actor-unix
    Package mirage-unikernel-actor-unix does not exist, create as a NEW package? [Y/n] y
    [mirage-unikernel-actor-unix.~dev] synchronised from file:///home/ehirdoy/src/light_actor/test/mirage
    mirage-unikernel-actor-unix is now pinned to file:///home/ehirdoy/src/light_actor/test/mirage (version ~dev)
    [ERROR] No solution for mirage-unikernel-actor-unix: The following dependencies couldn't be met:
              - mirage-unikernel-actor-unix → actor
                  unknown package
    
    Command failed: opam list --readonly --external  --resolve=mirage-unikernel-actor-unix returned 20
    Ok, mirage-unikernel-actor-unix is no longer pinned to file:///home/ehirdoy/src/light_actor/test/mirage (version ~dev)
    opam install -y --deps-only .
    The following dependencies couldn't be met:
      - mirage-unikernel-actor-unix → actor
          unknown package
    
    No solution found, exiting
    Makefile:14: recipe for target 'depend' failed
    make[1]: *** [depend] Error 20
    make[1]: Leaving directory '/home/ehirdoy/src/light_actor/test/mirage'
    Makefile.mine:4: recipe for target 'unix' failed
    make: *** [unix] Error 2
    
    Signed-off-by: Hiroshi Doyu <hiroshi.doyu@ericsson.com>

--- test/mirage/config.ml
+++ test/mirage/config.ml
@@ -4,7 +4,11 @@ let uuid =
   let doc = Key.Arg.info ~doc:"uuid of service" ["uuid"] in
   Key.(create "uuid" Arg.(opt string "server" doc))
 
-let main = foreign ~keys:[Key.abstract uuid] "Unikernel.Main" (stackv4 @-> job)
+
+let packages = [package "actor"; package "duration"]
+let main =
+  foreign ~packages ~keys:[Key.abstract uuid]
+    "Unikernel.Main" (stackv4 @-> job)
 
 let stack = generic_stackv4 default_network
 
