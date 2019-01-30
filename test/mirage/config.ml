a714ed282a2d79d73c1edea601e01101497f43d6
Hiroshi Doyu <hiroshi.doyu@ericsson.com>
2019-01-30 09:49:38 +0000

    add a new actor_mirage package

--- test/mirage/config.ml
+++ test/mirage/config.ml
@@ -5,7 +5,7 @@ let uuid =
   Key.(create "uuid" Arg.(opt string "server" doc))
 
 
-let packages = [package "actor"; package "duration"]
+let packages = [package "actor"; package "actor_mirage"; package "duration"]
 let main =
   foreign ~packages ~keys:[Key.abstract uuid]
     "Unikernel.Main" (stackv4 @-> job)
