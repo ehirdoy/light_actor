a79245a5bc19f437b650b37fd052df076be904ce
Hiroshi Doyu <hiroshi.doyu@ericsson.com>
2019-02-11 12:41:12 +0200

    docker: add support for MirageOS
    Mirage + Unix in docker-compose
    
    Signed-off-by: Hiroshi Doyu <hiroshi.doyu@ericsson.com>

--- run.sh
+++ run.sh
@@ -1,1 +1,25 @@
-/code/_build/default/test/actor_param_test_0.exe "$1"
+if [ "$#" -eq 1 ]; then
+    UUID="$1"
+elif [ -z "$UUID" ]; then
+    echo "Usage: $0 UUID" >&2
+    exit 123
+fi
+
+[ -z "$APP" ] && \
+    APP="/code/_build/default/test/actor_param_test_0.exe"
+
+IP=$(dig +short $UUID)
+[ -z "$IP" ] && (echo "No IP"; exit 123)
+SERVER_IP=$(dig +short "server")
+[ -z "$SERVER_IP" ] && (echo "No server IP"; exit 123)
+[ -z "$SERVER_PORT" ] && SERVER_PORT=5555
+[ -z "$PORT" ] && PORT=$SERVER_PORT
+
+if [ "$($APP --help > /dev/null 2>&1)" ]; then
+    APP+=" --uuid=$UUID --ip=$IP --port=$PORT \
+          --server-ip=$SERVER_IP --server-port=$SERVER_PORT"
+else
+    APP+=" $UUID"
+fi
+
+$APP
