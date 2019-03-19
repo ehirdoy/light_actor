a817efb663fe3b57d63b952e9cb3c33e9e12efe0
Hiroshi Doyu <hiroshi.doyu@ericsson.com>
2019-03-19 08:49:04 +0000

    OK, mirage/net: packet {de,}fragmentation
    Signed-off-by: Hiroshi Doyu <hiroshi.doyu@ericsson.com>

--- src/mirage/actor_net_mirage.ml
+++ src/mirage/actor_net_mirage.ml
@@ -7,7 +7,11 @@ open Lwt.Infix
 
 module Make (S : Mirage_stack_lwt.V4) = struct
 
-  type socket = S.TCPV4.flow
+  type socket = {
+    flow: S.TCPV4.flow;
+    len:  int;
+    bufs: bytes list;
+  }
 
   let prefix = "tcp://"
 
@@ -28,9 +32,34 @@ module Make (S : Mirage_stack_lwt.V4) = struct
 
   let exit () =
     Hashtbl.iter (fun _ v ->
-        Lwt.async (fun () -> S.TCPV4.close  v)
+        Lwt.async (fun () -> S.TCPV4.close v.flow)
       ) conn_pool;
     Lwt.return_unit
+
+  let pbuf_add addr flow bytes =
+    if Hashtbl.mem conn_pool addr then
+      let v = Hashtbl.find conn_pool addr in
+      (* assert (v.flow = flow);
+       * assert (v.len >= Bytes.length bytes); *)
+      Hashtbl.replace conn_pool addr
+        {flow=v.flow; len=v.len; bufs=bytes::v.bufs}
+    else
+      let len = Marshal.total_size bytes 0 in
+      Actor_log.debug "total_size=%d" len;
+      Hashtbl.add conn_pool addr
+        {flow=flow; len=len; bufs=[bytes]}
+
+  let pbuf_is_full addr =
+    let v = Hashtbl.find conn_pool addr in
+    let sum = List.fold_left (fun a el -> a + Bytes.length el) 0 v.bufs in
+    Actor_log.debug "packet %d / %d" sum v.len;
+    v.len = sum
+
+  let pbuf_bufs addr =
+    let v = Hashtbl.find conn_pool addr in
+    let bytes = Bytes.concat Bytes.empty (List.rev v.bufs) in
+    Hashtbl.remove conn_pool addr;
+    bytes
 
   let listen addr callback =
     let port = String.split_on_char ':' addr
@@ -41,12 +70,17 @@ module Make (S : Mirage_stack_lwt.V4) = struct
 
     let rec fn flow =
       let ip, port = S.TCPV4.dst flow in
+      let daddr = (Ipaddr.V4.to_string ip) ^ (string_of_int port) in
       S.TCPV4.read flow >>= function
       | Error e -> assert false
-      | Ok `Eof -> Actor_log.debug "EOF from %s:%d" (Ipaddr.V4.to_string ip) port; Lwt.return_unit
+      | Ok `Eof -> Actor_log.debug "EOF from %s" daddr; Lwt.return_unit
       | Ok (`Data buf) ->
-        Actor_log.debug "READ from %s:%d %d Bytes" (Ipaddr.V4.to_string ip) port (Cstruct.len buf);
-        callback (Cstruct.to_string buf) >>= fun () -> fn flow
+        Actor_log.debug "READ from %s %d Bytes" daddr (Cstruct.len buf);
+        pbuf_add daddr flow (Cstruct.to_bytes buf);
+        if pbuf_is_full daddr then
+          callback (Bytes.to_string (pbuf_bufs daddr)) >>= fun () -> fn flow
+        else
+          fn flow
     in
 
     S.listen_tcpv4 s ~port fn;
@@ -56,7 +90,10 @@ module Make (S : Mirage_stack_lwt.V4) = struct
     let%lwt flow =
       if Hashtbl.mem conn_pool addr then (
         Actor_log.debug "Found connection with %s" addr;
-        Lwt.return (Hashtbl.find conn_pool addr)
+        let v = Hashtbl.find conn_pool addr in
+        assert (v.len = 0);
+        assert ((List.length v.bufs) = 0);
+        Lwt.return v.flow
       ) else (
         let ip, port =
           match String.split_on_char ':' addr with
@@ -75,7 +112,7 @@ module Make (S : Mirage_stack_lwt.V4) = struct
             loop ()
           | Ok flow ->
             Actor_log.debug "create_connection with %s" addr;
-            Hashtbl.add conn_pool addr flow;
+            Hashtbl.add conn_pool addr {flow=flow;len=0;bufs=[]};
             Lwt.return flow
         in
         loop ()
